b62e74e44ff542966c816f748ca3abf8117477f8
xarkenz <seanedwardsclarke@gmail.com>
2024-11-17 04:55:45 -0600

    moved slider back to top

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ st.html("""
             background-color: white;
             z-index: 999;
             box-shadow: 0 0 1rem 1rem white;
+            padding-top: 1rem;
+            padding-bottom: 1rem;
         }
     </style>
 """)
@@ -24,29 +26,25 @@ DATE_RANGE_KEY = "date_range"
 def on_date_range_slider_change():
     # Surely there's a better way but I don't care
     st.session_state[DATE_RANGE_KEY] = st.session_state[DATE_RANGE_SLIDER_KEY]
-    
-with st.container(key="footer"):
-    # Double-ended datetime slider
-    start_date = datetime.today() - timedelta(days=365)
-    end_date = datetime.today()
-    if DATE_RANGE_KEY not in st.session_state:
-        st.session_state[DATE_RANGE_KEY] = (start_date, end_date)
-    st.slider(
-        "Select a date range",
-        key=DATE_RANGE_SLIDER_KEY,
-        min_value=start_date,
-        max_value=end_date,
-        value=st.session_state[DATE_RANGE_KEY],
-        step=timedelta(days=1),
-        on_change=on_date_range_slider_change,
-    )
 
+# Double-ended datetime slider
+start_date = datetime.today() - timedelta(days=365)
+end_date = datetime.today()
+if DATE_RANGE_KEY not in st.session_state:
+    st.session_state[DATE_RANGE_KEY] = (start_date, end_date)
+st.slider(
+    "Date range",
+    key=DATE_RANGE_SLIDER_KEY,
+    min_value=start_date,
+    max_value=end_date,
+    value=st.session_state[DATE_RANGE_KEY],
+    step=timedelta(days=1),
+    on_change=on_date_range_slider_change,
+)
+
+with st.container(key="footer"):
     status = st.status("Fetching latest information...")
     start_datetime = datetime.now()
-
-    # This is awful, but essentially it creates an invisible element with height 0
-    # so there's an extra flex gap between the status box and the bottom of the screen
-    st.container().container()
 
 well.well_listing()
 
