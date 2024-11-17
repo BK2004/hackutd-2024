8647659722a724e62bc7588f7d91899e927a8d20
BK2004 <thunder10.bk@gmail.com>
2024-11-17 02:47:21 -0600

    data within range for specific wells. change well structure

--- components/well.py
+++ components/well.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 import enum
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
+from services.data import well_names, getWellData, mark_anomalies
 import pandas
 
 class Status(enum.Enum):
@@ -26,16 +27,10 @@ def random_status():
     return choice([Status.OK, Status.OK, Status.OK, Status.OK, Status.OK, Status.OK, Status.OK, Status.OK, Status.HYDRATION_PREDICTED, Status.HYDRATION_DETECTED])
 
 class Well:
-    def __init__(self, path: str, status: Status):
-        self.path: str = path
-        self.name: str = path.split("/")[-1].split("_")[0]
-        self.data: pandas.DataFrame = None
+    def __init__(self, data: pandas.DataFrame, status: Status, name: str):
+        self.name: str = name
+        self.data: pandas.DataFrame = data
         self.status: Status = status
-        self.load_data()
-    
-    def load_data(self):
-        self.data = pandas.read_csv(self.path)
-        self.data = self.data.ffill()
 
 class WellList:
     def __init__(self):
@@ -44,13 +39,14 @@ class WellList:
 
 def fetch_well_data() -> WellList:
     wells = WellList()
-    for filename in os.listdir("data"):
-        if filename.endswith(".csv"):
-            well = Well("data/" + filename, random_status())
-            if well.status == Status.OK:
-                wells.regular.append(well)
-            else:
-                wells.priority.append(well)
+    for well_name in well_names:
+        data = getWellData(well_name, datetime.now() - timedelta(weeks=8), datetime.now())
+        mark_anomalies(data)
+        well = Well(data, random_status(), well_name) # TODO: Use small range of time based on interval
+        if well.status == Status.OK:
+            wells.regular.append(well)
+        else:
+            wells.priority.append(well)
     return wells
 
 def display_well(well: Well, expanded: bool):
@@ -62,7 +58,7 @@ def display_well(well: Well, expanded: bool):
             st.warning("Hydration predicted to occur soon", icon=icon)
         elif well.status == Status.HYDRATION_DETECTED:
             st.error("Hydration presence detected", icon=icon)
-        st.line_chart(well.data, x="Time")
+        st.scatter_chart(data=well.data, x="Time", y="Inst/Set/Valve", color="anomaly")
 
 @st.fragment(run_every=10)
 def well_listing():
--- services/data.py
+++ services/data.py
@@ -1,22 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 INST_COL = "Inj Gas Meter Volume Instantaneous"
 SETPOINT_COL = "Inj Gas Meter Volume Setpoint"
 VALVE_COL = "Inj Gas Valve Percent Open"
 
+well_names = []
+
 def readAll():
 	data = pd.DataFrame()
-	i = 0
 	for filename in os.listdir("./data"):
-		if i >= 5: break
-		i += 1
 		df = pd.read_csv("./data/" + filename)
+		well_name = filename[:filename.find('_')]
+		well_names.append(well_name)
+
+		df['Well'] = [well_name for _ in range(len(df['Time']))]
+
 		df = df.ffill().bfill()
 		df['Inst/Set/Valve'] = df[INST_COL]/df[SETPOINT_COL]/(df[VALVE_COL])
 		
 		data = pd.concat([data, df])
+	data['Time'] = pd.to_datetime(data['Time'])
 	
 	return data
 
@@ -28,8 +33,10 @@ def computeMeanSD(data: pd.DataFrame):
 data = readAll()
 (mean, sd) = computeMeanSD(data)
 
-data.plot.scatter(x='Time', y='Inst/Set/Valve')
-y = [mean - 2*sd for _ in range(len(data['Time']))]
-print(mean)
-plt.plot(data['Time'], y, color="red")
-plt.show()
+def getWellData(well, start_time, end_time):
+	ret_data = data.loc[(data['Time'] >= start_time) & (data['Time'] <= end_time) & (data['Well'] == well)]
+	return ret_data
+
+# Mark hydrates with anomaly col (red for hydrate, '' otherwise)
+def mark_anomalies(data: pd.DataFrame):
+	data["anomaly"] = np.where(data['Inst/Set/Valve'] <= mean - 2*sd, True, False)
